33df1266366c5306d87c0fe3ee27bf4ad5f1f931
kiaristello <ktelloruiz2010@gmail.com>
2025-07-09 17:59:27 -0500

    Configurando puerto Serial e imprimiendo cada vez que se mueve el servo motor

--- digital-input.ino
+++ digital-input.ino
@@ -8,6 +8,8 @@ int pinServo = 2;
 int pinLed = 4;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Iniciando programa");
   // Entradas digitales
   pinMode(pulsadorCeroGrados, INPUT_PULLUP);
   pinMode(pulsadorCientochentaGrados, INPUT_PULLUP);
@@ -22,11 +24,13 @@ void setup() {
 void loop() {
   // Configurando ángulos y leds
   if (digitalRead(pulsadorCeroGrados) == LOW){
+    Serial.println("Mi Servo se esta moviendo a 0°");
     myservo.write(0);
     digitalWrite(pinLed, HIGH);
   }
 
   if (digitalRead(pulsadorCientochentaGrados) == LOW){
+    Serial.println("Mi Servo se esta moviendo a 180°");
     myservo.write(180);
     digitalWrite(pinLed, LOW);
   }
