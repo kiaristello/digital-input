c32e3325bdfe667b8f23129e9ac36dacecbc53c3
kiaristello <ktelloruiz2010@gmail.com>
2025-07-14 17:53:10 -0500

    Incrementando cantidad de pulsadores

--- digital-input.ino
+++ digital-input.ino
@@ -1,20 +1,28 @@
 #include <Servo.h>
 Servo myservo;
 
-int pulsadorCeroGrados = 7; 
-int pulsadorCientochentaGrados = 6;
+int pulsador1 = 6; 
+int pulsador2 = 7;
+int pulsador3 = 8;
+int pulsador4 = 9;
+int pulsador5 = 10;
 int pinServo = 2;
 int pinLed = 4;
 boolean encendido = false;
 
 void setup() {
+  encendido = false;
   Serial.begin(9600);
   Serial.println("Iniciando programa");
   // Entradas digitales
-  pinMode(pulsadorCeroGrados, INPUT_PULLUP);
-  pinMode(pulsadorCientochentaGrados, INPUT_PULLUP);
-  pinMode(pinLed, OUTPUT);
+  pinMode(pulsador1, INPUT_PULLUP);
+  pinMode(pulsador2, INPUT_PULLUP);
+  pinMode(pulsador3, INPUT_PULLUP);
+  pinMode(pulsador4, INPUT_PULLUP);
+  pinMode(pulsador5, INPUT_PULLUP);
 
+  // Salidas digitales
+  pinMode(pinLed, OUTPUT);
 
   // Configurando Servo
   myservo.attach(pinServo);
@@ -22,28 +30,47 @@ void setup() {
 }
 
 void loop() {
-  if (encendido == "false"){
+  if (encendido == false){
     Serial.println("Introducir palabra secreta");
     while (Serial.available() == 0);
     String comando = Serial.readString();
     if (comando == 'encender'){
+      Serial.println("Encendiendo circuito");
       encendido = true;
     }else{
       Serial.println("NO ERES TU 👀");
     }
   } else {
     // Configurando ángulos y leds
-    if (digitalRead(pulsadorCeroGrados) == LOW){
-      Serial.println("Mi Servo se esta moviendo a 0°");
-      myservo.write(0);
-      digitalWrite(pinLed, HIGH);
-    }
+      if (digitalRead(pulsador1) == LOW){
+        Serial.println("Mi Servo se esta moviendo a 0°");
+        myservo.write(0);
+        digitalWrite(pinLed, HIGH);
+      }
 
-    if (digitalRead(pulsadorCientochentaGrados) == LOW){
-      Serial.println("Mi Servo se esta moviendo a 180°");
-      myservo.write(180);
-      digitalWrite(pinLed, LOW);
+      if (digitalRead(pulsador2) == LOW){
+        Serial.println("Mi Servo se esta moviendo a 45°");
+        myservo.write(45);
+        digitalWrite(pinLed, LOW);
+      }
+          
+      if (digitalRead(pulsador3) == LOW){
+        Serial.println("Mi Servo se esta moviendo a 90°");
+        myservo.write(90);
+        digitalWrite(pinLed, HIGH);
+      }
+
+      if (digitalRead(pulsador4) == LOW){
+        Serial.println("Mi Servo se esta moviendo a 135°");
+        myservo.write(135);
+        digitalWrite(pinLed, LOW);
+      }    
+      
+      if (digitalRead(pulsador5) == LOW){
+        Serial.println("Mi Servo se esta moviendo a 180°");
+        myservo.write(180);
+        digitalWrite(pinLed, HIGH);
+      }
     }
-  }
 
 }
